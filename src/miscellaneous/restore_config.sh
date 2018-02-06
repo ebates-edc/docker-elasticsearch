eec3dd887b3391103171b33c19dc21d98c013223
Roman Kournjaev <kournjaev@gmail.com>
2018-02-06 11:52:03 -0800

    Initial commit

---
+++ src/miscellaneous/restore_config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mkdir -p /elasticsearch/config
+
+if [ ! -f /elasticsearch/config/elasticsearch.yml ]; then
+    cp -r /.backup/elasticsearch/config/elasticsearch.yml /elasticsearch/config/
+fi
+
+if [ ! -f /elasticsearch/config/log4j2.properties ]; then
+    cp -r /.backup/elasticsearch/config/log4j2.properties /elasticsearch/config/
+fi
+
+if [ ! -f /elasticsearch/config/jvm.options ]; then
+    cp -r /.backup/elasticsearch/config/jvm.options /elasticsearch/config/
+fi
+
+rsync -av /.backup/elasticsearch/config/searchguard /elasticsearch/config
